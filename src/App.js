ba05a3f9111a018dd7ce19101e2681615261b52f
jeremy <jeremy@jeremybeck.io>
2021-08-31 16:57:52 -0700

    input validation and security issues

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import Particles from 'react-particles-js';
-import Clarifai from 'clarifai';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import Rank from './components/Rank/Rank';
@@ -9,10 +8,6 @@ import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import Signin from './components/Signin/Signin';
 import Register from './components/Register/Register';
 import './App.css';
-
-const app = new Clarifai.App({
-  apiKey: ''
-})
 
 const particlesOptions = {
   particles: {
@@ -91,8 +86,16 @@ class App extends Component {
   }
 
   onButtonSubmit = () => {
+    console.log(this.state.input);
     this.setState({ imageUrl: this.state.input })
-    app.models.predict('f76196b43bbd45c99b4f3cd8e8b40a8a', this.state.input)
+      fetch('http://localhost:3000/imageurl', {
+        method: 'post',
+        headers: { 'Content-Type': 'application/json'},
+        body: JSON.stringify({
+          input: this.state.input
+        })
+      })
+      .then(response => response.json())
       .then(response => {
         if (response) {
           fetch('http://localhost:3000/image', {
