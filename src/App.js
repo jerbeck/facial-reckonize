ab38d064e72c29e153076a54434e6d4aa2bbeee6
jeremy <jeremy@jeremybeck.io>
2021-08-30 18:07:16 -0700

    Mostly functional with backend connectivity

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import Register from './components/Register/Register';
 import './App.css';
 
 const app = new Clarifai.App({
-  apiKey: 'bb68dec5ecf348658a642c3a4be4dbb2'
+  apiKey: ''
 })
 
 const particlesOptions = {
@@ -35,8 +35,27 @@ class App extends Component {
       box: {},
       route: 'signin',
       isSignedIn: false,
+      user: {
+        id: '',
+        name: '',
+        email: '',
+        entries: 0,
+        joined: '',
+      }
     }
   }
+
+  loadUser = (data) => {
+    this.setState({ user: {
+      id: data.id,
+      name: data.name,
+      email: data.email,
+      entries: data.entries,
+      joined: data.joined
+      }
+    })
+  }
+
 
   calculateFaceLocation = (data) => {
     const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
@@ -52,17 +71,33 @@ class App extends Component {
       bottomRow: height - (clarifaiFace.bottom_row * height),
     }
 
-    this.setState({box: newBox})
+    this.setState({ box: newBox })
   }
 
   onInputChange = (event) => {
-    this.setState({ input: event.target.value});
+    this.setState({ input: event.target.value });
   }
 
   onButtonSubmit = () => {
     this.setState({ imageUrl: this.state.input })
     app.models.predict('f76196b43bbd45c99b4f3cd8e8b40a8a', this.state.input)
-      .then(response => this.calculateFaceLocation(response))
+      .then(response => {
+        if (response) {
+          fetch('http://localhost:3000/image', {
+            method: 'put',
+            headers: { 'Content-Type': 'application/json'},
+            body: JSON.stringify({
+              id: this.state.user.id
+            })
+          })
+            .then(response => response.json())
+            .then(count => {
+              this.setState(Object.assign(this.state.user, { entries: count }));
+            })
+        }
+        this.calculateFaceLocation(response);
+
+      })
       .catch(err => console.log(err));
   }
 
@@ -89,7 +124,10 @@ class App extends Component {
         { route === 'home' ?
             <div>
               <Logo />
-              <Rank />
+              <Rank 
+                name={this.state.user.name}
+                entries={this.state.user.entries}
+              />
               <ImageLinkForm 
                 onInputChange={this.onInputChange}
                 onButtonSubmit={this.onButtonSubmit}
@@ -98,8 +136,14 @@ class App extends Component {
             </div>  
           : (
             route === 'signin'
-              ? <Signin onRouteChange={this.onRouteChange}/>
-              : <Register onRouteChange={this.onRouteChange} />
+              ? <Signin 
+                  onRouteChange={this.onRouteChange}
+                  loadUser={this.loadUser}
+                />
+              : <Register 
+                  onRouteChange={this.onRouteChange} 
+                  loadUser={this.loadUser}
+                />
             )
         }
       </div>
